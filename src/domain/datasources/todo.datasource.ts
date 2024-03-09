4db857046bc33af7bf22e5ca2255e59c5bbc2eef
Dieguidev <diegogaraycullas@gmail.com>
2024-03-08 21:37:10 -0500

    creados en domain todo.datasource.ts y todo.repository.ts para respetar clean architecture

---
+++ src/domain/datasources/todo.datasource.ts
@@ -0,0 +1,22 @@
+import { CreateTodoDto, UpdateTodoDto } from "../dtos";
+import { TodoEntity } from "../entities/todo.entity";
+
+
+
+export abstract class TodoDatasource {
+
+  abstract create(createTodoDto:CreateTodoDto): Promise<TodoEntity>;
+
+  //todo: paginacion
+  abstract getAll(): Promise<TodoEntity[]>;
+
+  abstract getById(id: number): Promise<TodoEntity>;
+
+  abstract update(id: number, updateTodoDto: UpdateTodoDto): Promise<TodoEntity>;
+
+  abstract delete(id: number): Promise<TodoEntity>;
+
+
+
+
+}
